aa4a50179e14121f5d73f3bb899fd92981dbe41b
gandalfTheGreyWizard <chakraborty.soumik23@gmail.com>
2026-05-22 23:50:18 +0530

    removed the old nestjs backend and test app frontend repos

--- a_test_app/src/app/jwt-interceptor.interceptor.ts
+++
@@ -1,9 +0,0 @@
-import { HttpInterceptorFn } from '@angular/common/http';
-
-export const jwtInterceptorInterceptor: HttpInterceptorFn = (req, next) => {
-  const bearerToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImdhbmRhbGYiLCJ0eXBlIjoid2l6YXJkIiwiZXhwIjoxNzUxNjQ5OTA2Ljc5MTk2NH0.KDZ7kQl6pWSzVYALkGN30wYZaDr6iGRh17rWkkfgepI"
-  const reqWithJwtHeaders = req.clone({
-    headers: req.headers.set('Authorization', `Bearer ${bearerToken}`)
-  });
-  return next(reqWithJwtHeaders);
-};
--- rss_backend/src/dtos/user.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface UserInterface {
-  name: string;
-  type: string;
-}
