9e1033a1d8aeff474ca0f5cc2be1e061de9482b9
Gandlf The Grey <chakraborty.soumik23@gmail.com>
2026-08-02 20:15:03 +0530

    certain changes to standalone scripts

--- python_scripts/.ycm_extra_conf.py
+++ python_scripts/.ycm_extra_conf.py
@@ -1,5 +1,5 @@
 def Settings( **kwargs ):
     return {
-        'intrepreter_path': '/home/gandalf/codes/feedr_ecosystem/python_scripts/venv/bin/python',
-        'sys_path': ['/home/gandalf/codes/feedr_ecosystem/python_scripts/venv/lib/python3.12/site-packages']
+        'intrepreter_path': '/home/gandalf/code/feedr_ecosystem/python_scripts/venv/bin/python',
+        'sys_path': ['/home/gandalf/code/feedr_ecosystem/python_scripts/venv/lib/python3.13/site-packages']
     }
--- python_scripts/standalone_scripts/redis_test.py
+++ python_scripts/standalone_scripts/redis_test.py
@@ -1,6 +1,6 @@
 import redis
 try:
-    r = redis.Redis(host='localhost', port=6379, db=0, legacy_responses=False)
+    r = redis.Redis(host='localhost', port=6666, db=0, legacy_responses=False)
     r.set('foo', 'bar');
     print(r.get('foo'));
 except Exception as err:
